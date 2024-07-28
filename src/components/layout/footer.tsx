7c2f8a8f7b817fe8123378639c87fab3a1725144
NicoBocq <nicolas.fow@gmail.com>
2024-07-28 13:07:27 +0200

    Added desc

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,15 +1,26 @@
 import Link from 'next/link'
 
 import Icon from '../custom-ui/icon'
+import { buttonVariants } from '../ui/button'
 
 export default function Footer() {
   return (
     <footer>
-      <div className="mx-auto flex max-w-7xl items-center justify-end px-4 py-2 lg:px-8">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 lg:px-8">
+        <p className="text-sm text-muted-foreground">
+          Forecast API:{' '}
+          <Link
+            href="https://open-meteo.com/"
+            target="_blank"
+            className={buttonVariants({ variant: 'link' })}
+          >
+            Open Meteo
+          </Link>
+        </p>
         <Link
           href="https://github.com/nicobocq"
           target="_blank"
-          className="inline-flex items-center text-sm text-muted-foreground hover:text-muted-foreground/80"
+          className={buttonVariants({ variant: 'link' })}
         >
           <Icon name="Github" margin="right" />
           nicobocq
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { auth } from '@/auth'
 
-import { Button } from '../ui/button'
+import { Button, buttonVariants } from '../ui/button'
 import UserMenu from '../user/menu'
 
 export default async function Header() {
@@ -12,12 +12,24 @@ export default async function Header() {
         aria-label="Global"
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
       >
-        <Link href="/">
-          <h1 className="inline-flex items-center text-3xl font-extrabold">
-            mumu
-            <span className="text-primary">midex</span>
-          </h1>
-        </Link>
+        <div>
+          <Link href="/">
+            <h1 className="inline-flex items-center text-3xl font-extrabold">
+              mumu
+              <span className="text-primary">midex</span>
+            </h1>
+          </Link>
+          <p className="text-sm text-primary/70">
+            Your cities ranked by{' '}
+            <Link
+              href="https://en.wikipedia.org/wiki/Humidex"
+              target="_blank"
+              className={buttonVariants({ variant: 'link' })}
+            >
+              humidex
+            </Link>
+          </p>
+        </div>
         <div>
           {!session ? (
             <Link href="/login">
