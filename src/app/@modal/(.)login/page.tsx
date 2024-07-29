a374a4d575afddbc0e3597656e0a7c1b1f22f4fc
NicoBocq <nicolas.fow@gmail.com>
2024-07-29 11:02:01 +0200

    Synced to app config

--- src/app/@modal/(.)login/page.tsx
+++ src/app/@modal/(.)login/page.tsx
@@ -1,11 +1,12 @@
 import SignInButton from '@/components/auth/oauth-button'
 import Modal from '@/components/custom-ui/modal'
+import { app } from '@/config/app'
 
 export default function Page() {
   return (
     <Modal
-      title="Log in"
-      description="Customize forecast list and many more to come (or not)!"
+      title={app.login.title}
+      description={app.login.description}
       open
       footer={<SignInButton />}
     />
