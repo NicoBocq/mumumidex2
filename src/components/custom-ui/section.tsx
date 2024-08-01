f54f5ab66eaa2cbcca48ca59eeb6a87038266b61
NicoBocq <nicolas.fow@gmail.com>
2024-08-01 10:55:31 +0200

    Wip

--- src/components/custom-ui/section.tsx
+++ src/components/custom-ui/section.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react'
 
 export default function Section({ children }: { children: React.ReactNode }) {
   return (
-    <div 
-    className="flex min-h-[calc(100svh-140px)] flex-col items-center justify-center gap-4">
-      {children}</div>
-  );
+    <div className="flex min-h-[calc(100svh-140px)] flex-col items-center justify-center gap-4">
+      {children}
+    </div>
+  )
 }
