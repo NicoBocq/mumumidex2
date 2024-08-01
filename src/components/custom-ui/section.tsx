830f71abd59069482397218c91329c065b9ee379
NicoBocq <nicolas.fow@gmail.com>
2024-08-01 10:22:00 +0200

    init section

--- src/components/custom-ui/section.tsx
+++ src/components/custom-ui/section.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Section({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-[calc(100svh-140px)] flex-col items-center justify-center gap-4">
-      {children}
-    </div>
+    <div 
+    className="flex min-h-[calc(100svh-140px)] flex-col items-center justify-center gap-4">
+      {children}</div>
   );
 }
