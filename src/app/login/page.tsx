0ac2fefca12d0ff97b73cec002339f2922755060
NicoBocq <nicolas.fow@gmail.com>
2026-02-01 12:18:50 +0100

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,23 +3,36 @@ import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 import OAuthButton from '@/components/auth/oauth-button'
+import { buttonVariants } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { app } from '@/config/app'
+import { cn } from '@/lib/utils'
+import { ArrowLeft } from 'lucide-react'
+import Link from 'next/link'
 
 export default async function Page() {
   const session = await auth.api.getSession({ headers: await headers() })
   if (session) redirect('/')
   return (
-    <div className="flex min-h-[300px] items-center justify-center">
-      <Card className="w-full max-w-sm border-0 bg-primary/5">
-        <CardHeader>
-          <CardTitle className="text-primary">{app.login.title}</CardTitle>
-          <CardDescription>{app.login.description}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <OAuthButton />
-        </CardContent>
-      </Card>
+    <div className="flex min-h-dvh flex-col items-center justify-center gap-4 p-4">
+      <div className="flex flex-col gap-4 min-w-lg">
+        <Link
+          href="/"
+          className={cn(buttonVariants({ variant: 'ghost', size: 'sm' }), 'self-start')}
+        >
+          <ArrowLeft className="mr-2 h-4 w-4" />
+          Back
+        </Link>
+        <Card className="w-full max-w-sm border-0 bg-primary/5">
+          <CardHeader>
+            <CardTitle className="text-primary">{app.login.title}</CardTitle>
+            <CardDescription>{app.login.description}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <OAuthButton />
+          </CardContent>
+        </Card>
+      </div>
     </div>
   )
 }
